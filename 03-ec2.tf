a71405afc7d4018cf2bd540cb2f1d7e1c38faf23
Dan <maor.dan@gmail.com>
2024-03-11 17:46:27 +0200

    initial commit

--- 03-ec2.tf
+++ 03-ec2.tf
@@ -24,22 +24,6 @@ resource "aws_security_group" "instances_sg" {
   }
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3a.micro"
@@ -63,7 +47,7 @@ resource "aws_instance" "web" {
       "    listen 80;",
       "    server_name _;",
       "    location / {",
-      "        proxy_pass http://your_kubernetes_cluster_endpoint;",
+      "        proxy_pass http://###your_kubernetes_cluster_endpoint###;",
       "        proxy_set_header Host $host;",
       "        proxy_set_header X-Real-IP $remote_addr;",
       "        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;",
